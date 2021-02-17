9d998a1337912267dbe06a849e37f98f89e9a574
Timothy-KJM <vwmartin@naver.com>
2021-02-17 20:51:06 +0900

    I modified some codes.

--- ConditionC5.c
+++ ConditionC5.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 void menu(void) {
@@ -14,17 +14,17 @@ void menu(void) {
 
 void select_menu(char *select) {
 	if (strcmp(select,"apple") == 0) printf("your choice is apple");
-	else if (strcmp(select,"orange")==0) printf("your choice is orange");
+	else if (strcmp(select,"orange")== 0) printf("your choice is orange");
 	else if (strcmp(select, "banana") == 0) printf("your choice is banana");
-	else if (strcmp(select, "peach")==0) printf("your choice is peach");
+	else if (strcmp(select, "peach")== 0) printf("your choice is peach");
 }
 
 int main(void) {
-	char *select
+	char select[20];
 
 	menu();
 	printf("주문하실 메뉴를 입력해주세요: ");
-	scanf_s("%s", select);
-	select_menu(*select);
+	scanf_s("%s", select, sizeof(select));
+	select_menu(select);
 	return 0;
-}*/
+}
